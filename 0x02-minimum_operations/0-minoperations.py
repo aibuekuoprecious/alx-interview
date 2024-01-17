3ff949603ccf775f7c9dc902fd9e83f4e8faf8ff
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-17 14:32:26 +0100

    updated

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ Module for 0-minoperations """
 
+
 def minOperations(n):
     """ Fewest number of operations needed """
     if (n < 2):
         return 0
-    
+
     operations = 0
     currentChar = 1
     lastCopied = 1
@@ -15,7 +16,7 @@ def minOperations(n):
             # Copy All
             lastCopied = currentChar
             operations += 1
-            
+
         # Paste
         currentChar += lastCopied
         operations += 1
--- 0x02-minimum_operations/README.md
+++ 0x02-minimum_operations/README.md
@@ -1,1 +1,9 @@
 # 0x02-minimum_operations
+
+This Python script defines a function called `minOperations` that calculates the fewest number of operations needed to reach a given number 'n' using a copy and paste mechanism. 
+
+If 'n' is less than 2, the function returns 0.
+
+Otherwise, it iteratively performs operations to reach 'n' using a copy and paste mechanism. 
+
+The function counts the operations and returns the total number of operations needed.
