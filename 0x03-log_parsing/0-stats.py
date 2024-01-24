a3d43e03136ce395d0fb7dde9459ad9670a3db6f
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-24 10:51:06 +0100

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,32 @@
 #!/usr/bin/python3
-
 import sys
+import signal
 
+def print_stats(status_codes, file_size):
+    print("File size: {:d}".format(file_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{:s}: {:d}".format(code, status_codes[code]))
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
+def signal_handler(sig, frame):
+    print_stats(status_codes, file_size)
 
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+signal.signal(signal.SIGINT, signal_handler)
 
-
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+file_size = 0
 
 try:
-    for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
-
-        if len(parsed_line) > 2:
-            counter += 1
-
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
+    for i, line in enumerate(sys.stdin, 1):
+        split_line = line.split()
+        if len(split_line) < 2:
+            continue
+        if split_line[-2] in status_codes:
+            status_codes[split_line[-2]] += 1
+        file_size += int(split_line[-1])
+        if i % 10 == 0:
+            print_stats(status_codes, file_size)
+except KeyboardInterrupt:
+    pass
 finally:
-    print_msg(dict_sc, total_file_size)
+    print_stats(status_codes, file_size)
