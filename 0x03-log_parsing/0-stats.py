0fbcdf3c98c1724e3deebddb1bcb3867dc711475
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-24 09:55:22 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,46 @@
+#!/usr/bin/python3
 
 import sys
 
 def print_stats(total_size, status_codes):
-    print("File size: {}".format(total_size))
+    print("Total file size: File size:", total_size)
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+            print(code, ":", status_codes[code])
 
-def parse_line(line):
+def parse_log():
+    total_size = 0
+    status_codes = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    }
+    count = 0
+
     try:
-        parts = line.split()
-        size = int(parts[-1])
-        code = int(parts[-2])
-        return size, code
-    except (ValueError, IndexError):
-        return None, None
+        for line in sys.stdin:
+            count += 1
+            if count > 10:
+                print_stats(total_size, status_codes)
+                count = 1
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+            parts = line.split()
+            if len(parts) >= 7:
+                size = int(parts[-1])
+                code = int(parts[-2])
+                total_size += size
+                if code in status_codes:
+                    status_codes[code] += 1
 
-try:
-    for line in sys.stdin:
-        size, code = parse_line(line)
-        if size is not None and code is not None:
-            total_size += size
-            if code in status_codes:
-                status_codes[code] += 1
+    except KeyboardInterrupt:
+        pass
 
-        line_count += 1
-        if line_count % 10 == 0:
-            print_stats(total_size, status_codes)
-
-except KeyboardInterrupt:
     print_stats(total_size, status_codes)
-    raise
+
+if __name__ == "__main__":
+    parse_log()
