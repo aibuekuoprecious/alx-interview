f18d348c8b398726cbb5d5dbcf6f926e59e494e1
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-24 10:53:40 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,16 +15,18 @@ signal.signal(signal.SIGINT, signal_handler)
 
 status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 file_size = 0
+line_count = 0
 
 try:
-    for i, line in enumerate(sys.stdin, 1):
+    for line in sys.stdin:
         split_line = line.split()
         if len(split_line) < 2:
             continue
         if split_line[-2] in status_codes:
             status_codes[split_line[-2]] += 1
         file_size += int(split_line[-1])
-        if i % 10 == 0:
+        line_count += 1
+        if line_count % 10 == 0:
             print_stats(status_codes, file_size)
 except KeyboardInterrupt:
     pass
