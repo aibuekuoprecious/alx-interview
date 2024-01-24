cec4df36be04cdee393ff240394d2c25aeb5be20
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-24 10:58:45 +0100

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,18 +2,29 @@
 import sys
 import signal
 
+
 def print_stats(status_codes, file_size):
     print("File size: {:d}".format(file_size))
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
             print("{:s}: {:d}".format(code, status_codes[code]))
 
+
 def signal_handler(sig, frame):
     print_stats(status_codes, file_size)
 
+
 signal.signal(signal.SIGINT, signal_handler)
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0}
 file_size = 0
 line_count = 0
 
--- 0x03-log_parsing/README.md
+++ 0x03-log_parsing/README.md
@@ -1,1 +1,3 @@
-# 0x03-log_parsing
+# Log Parsing
+
+This project contains a script that reads stdin line by line and computes metrics based on the input format.
