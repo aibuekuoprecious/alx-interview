6d078fcf4ae0906cb643663a7cd4fd9b90a1902e
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-31 15:34:32 +0100

    utf8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
+
+
+def validUTF8(data):
+    """
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
+
+    Args:
+        data (list[int]): The data set to be checked.
+
+    Returns:
+        bool: True if the data set represents a valid UTF-8 encoding,
+              False otherwise.
+    """
+    num_bytes = 0
+
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for byte in data:
+        mask_byte = 1 << 7
+
+        if num_bytes == 0:
+            while mask_byte & byte:
+                num_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (byte & mask_1 and not (byte & mask_2)):
+                return False
+
+        num_bytes -= 1
+
+    return num_bytes == 0
