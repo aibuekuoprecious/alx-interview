089ef8257073c0ca9ee3065021b1a067105a94b3
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-02-11 06:15:43 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,28 +2,30 @@
 import sys
 
 
+def can_place(pos, taken_pos):
+    for i in range(len(taken_pos)):
+        if (
+            taken_pos[i] == pos
+            or taken_pos[i] - i == pos - len(taken_pos)
+            or taken_pos[i] + i == pos + len(taken_pos)
+        ):
+            return False
+    return True
+
+
+def place_queens(n, index, taken_pos, all_taken_pos):
+    if index == n:
+        all_taken_pos.append(taken_pos[:])
+        return
+
+    for i in range(n):
+        if can_place(i, taken_pos):
+            taken_pos.append(i)
+            place_queens(n, index + 1, taken_pos, all_taken_pos)
+            taken_pos.pop()
+
+
 def solveNQueens(n):
-    def can_place(pos, taken_pos):
-        for i in range(len(taken_pos)):
-            if (
-                taken_pos[i] == pos
-                or taken_pos[i] - i == pos - len(taken_pos)
-                or taken_pos[i] + i == pos + len(taken_pos)
-            ):
-                return False
-        return True
-
-    def place_queens(n, index, taken_pos, all_taken_pos):
-        if index == n:
-            all_taken_pos.append(taken_pos[:])
-            return
-
-        for i in range(n):
-            if can_place(i, taken_pos):
-                taken_pos.append(i)
-                place_queens(n, index + 1, taken_pos, all_taken_pos)
-                taken_pos.pop()
-
     all_taken_pos = []
     place_queens(n, 0, [], all_taken_pos)
     return all_taken_pos
