2dc60ecf081ac5b3fc949863300b3132891760c7
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-02-16 05:40:21 +0100

    0x06-starwars_api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const getFilmCharacters = async (filmId) => {
+  try {
+    const filmResponse = await requestPromise('https://swapi-api.hbtn.io/api/films/' + filmId);
+    const filmData = JSON.parse(filmResponse);
+    const characters = filmData.characters;
+    await printCharactersInExactOrder(characters, 0);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const printCharactersInExactOrder = async (characters, index) => {
+  if (index === characters.length) return;
+  try {
+    const characterResponse = await requestPromise(characters[index]);
+    const characterData = JSON.parse(characterResponse);
+    console.log(characterData.name);
+    await printCharactersInExactOrder(characters, index + 1);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const requestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+};
+
+getFilmCharacters(process.argv[2]);
