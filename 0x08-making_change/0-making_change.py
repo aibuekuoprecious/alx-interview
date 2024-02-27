5fb6d68164e2f16490d6316c716ba30bbec5edca
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-02-27 20:44:33 +0100

    updated

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,33 +1,22 @@
 #!/usr/bin/python3
-"""
-0. Change comes from within
+
+"""Making Change
 """
 
 
 def makeChange(coins, total):
+    """Given a list of coins and a total, return the minimum number of coins
     """
-    Calculates the minimum number of coins needed to make the given total amount.
-
-    Args:
-        coins (list): List of coin denominations.
-        total (int): Total amount to be made.
-
-    Returns:
-        int: Minimum number of coins needed to make the total amount. 
-        Returns -1 if it is not possible to make the total amount with the given coins.
-    """
-    
-    if total <= 0:
-        return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    if dp[total] == float('inf'):
+    if not coins:
         return -1
 
-    return dp[total]
+    num_coins = 0
+    coins = sorted(coins, reverse=True)
+    for coin in coins:
+        num_coins += total // coin
+        total %= coin
+
+    if total == 0:
+        return num_coins
+    else:
+        return -1
