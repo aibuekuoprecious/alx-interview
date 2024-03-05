5001cf9a8cd24d362cae8ef99df977c6f96316be
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-03-05 17:57:13 +0100

    updated

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,23 +10,14 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(num_rows):
         for j in range(num_cols):
-            assert grid[i][j] == 0 or grid[i][j] == 1
-            if i - 1 < 0:
-                perimeter += 1
-            else:
-                perimeter += grid[i - 1][j]
-            if j - 1 < 0:
-                perimeter += 1
-            else:
-                perimeter += grid[i][j - 1]
-
-            try:
-                perimeter += grid[i + 1][j]
-            except IndexError:
-                perimeter += 1
-            try:
-                perimeter += grid[i][j + 1]
-            except IndexError:
-                perimeter += 1
+            if grid[i][j] == 1:  # Only consider land cells
+                if i == 0 or grid[i - 1][j] == 0:  # Check upper neighbor
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:  # Check left neighbor
+                    perimeter += 1
+                if i == num_rows - 1 or grid[i + 1][j] == 0:  # Check lower neighbor
+                    perimeter += 1
+                if j == num_cols - 1 or grid[i][j + 1] == 0:  # Check right neighbor
+                    perimeter += 1
 
     return perimeter
