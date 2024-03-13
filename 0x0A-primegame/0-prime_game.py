5ab6adf6054642e5d40e5042c64f2f6109e2a2d3
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-03-13 13:20:35 +0100

    testing

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,42 +1,39 @@
 #!/usr/bin/python3
-"""0. Prime Game - Maria and Ben are playing a game"""
+"""Prime Game: Maria and Ben are playing a game"""
 
 
-def isWinner(x, nums):
-    """x - rounds
-    nums - numbers list
-    """
-    if x <= 0 or nums is None:
-        return None
-    if x != len(nums):
+def isWinner(rounds, nums):
+    """Determines the winner of the game."""
+    if rounds <= 0 or nums is None or rounds != len(nums):
         return None
 
-    ben = 0
-    maria = 0
+    ben_wins = 0
+    maria_wins = 0
 
-    a = [1 for x in range(sorted(nums)[-1] + 1)]
-    a[0], a[1] = 0, 0
-    for i in range(2, len(a)):
-        rm_multiples(a, i)
+    primes = [1] * (max(nums) + 1)
+    primes[0], primes[1] = 0, 0
 
-    for i in nums:
-        if sum(a[0:i + 1]) % 2 == 0:
-            ben += 1
+    for i in range(2, len(primes)):
+        remove_multiples(primes, i)
+
+    for num in nums:
+        if sum(primes[:num + 1]) % 2 == 0:
+            ben_wins += 1
         else:
-            maria += 1
-    if ben > maria:
+            maria_wins += 1
+
+    if ben_wins > maria_wins:
         return "Ben"
-    if maria > ben:
+    elif maria_wins > ben_wins:
         return "Maria"
-    return None
+    else:
+        return None
 
 
-def rm_multiples(ls, x):
-    """removes multiple
-    of primes
-    """
-    for i in range(2, len(ls)):
+def remove_multiples(lst, x):
+    """Removes multiples of primes."""
+    for i in range(2, len(lst)):
         try:
-            ls[i * x] = 0
-        except (ValueError, IndexError):
+            lst[i * x] = 0
+        except IndexError:
             break
