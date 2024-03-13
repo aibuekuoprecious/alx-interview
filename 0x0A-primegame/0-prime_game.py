08c4617934bb9ece82b7e332b39514c79982f750
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-03-13 13:16:08 +0100

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-"""0. Prime Game - Maria and Ben are playing a game"""
-
+"""Prime Game"""
 
 def isWinner(x, nums):
-    """x - rounds
-    nums - numbers list
+    """Determines the winner of a prime game.
     """
+
     if x <= 0 or x != len(nums) or nums is None:
         return None
-    
-    def isWinner(x, nums):
-        ben = 0
-        maria = 0
 
-        for num in nums:
-            if isPrime(num):
-                ben += 1
-            else:
-                maria += 1
+    ben = 0
+    maria = 0
 
-        if ben > maria:
-            return "Ben"
-        elif maria > ben:
-            return "Maria"
+    for num in nums:
+        if isPrime(num):
+            ben += 1
         else:
-            return None
+            maria += 1
 
+    if ben > maria:
+        return "Ben"
+    elif maria > ben:
+        return "Maria"
+    else:
+        return None
 
-    def isPrime(num):
-        if num < 2:
+def isPrime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
             return False
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
+    return True
